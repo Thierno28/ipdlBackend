20b34f914f57d2bd961cce38f26d14886ffe160c
Thierno28 <thiernodiop@esp.sn>
2022-06-21 20:53:49 +0000

    maj

--- models/user.js
+++ models/user.js
@@ -44,7 +44,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign(
-    { _id: this._id, isAdmin: this.isAdmin },
+    { _id: this._id},
     config.get("jwtPrivateKey")
   );
   return token;
